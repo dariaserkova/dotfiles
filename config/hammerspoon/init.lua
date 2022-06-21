cc7757bf6758741f9989f917ced06f2a2218f2a0
Daria Serkova <29811291+dariaserkova@users.noreply.github.com>
2022-06-21 14:07:35 +0300

    Changed lockscreen shortkey

--- config/hammerspoon/init.lua
+++ config/hammerspoon/init.lua
@@ -4,5 +4,5 @@ require "apps"
 require "grid"
 
 hs.hotkey.bind(mash, "r", function() hs.reload(); end)
-hs.hotkey.bind(mash, "a", function() hs.caffeinate.lockScreen(); end)
+hs.hotkey.bind(mash, "l", function() hs.caffeinate.lockScreen(); end)
 hs.alert("Hammerspoon config loaded")
